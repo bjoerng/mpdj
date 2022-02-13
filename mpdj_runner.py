1ef06d5372d678437201abb45b8a4213dad38e4f
Björn Gräbe <bjoern.graebe@studium.fernuni-hagen.de>
2022-02-13 21:55:37 +0100

    Keeping running playlist looks done.

--- mpdj_runner.py
+++ mpdj_runner.py
@@ -11,34 +11,38 @@ import argparse
 import jsonpickle
 from control.mpdj_runner_v2 import MPDJRunnerV2
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', help="The MPDJ file to run",required=True)
-parser.add_argument('-A', help="The HOST IP address to connect to")
+parser.add_argument('-f', '--file', help="The MPDJ file to run",required=True)
+parser.add_argument('-A', '--address', help="The HOST IP address to connect to")
 #parser.add_argument('-A', '--HOST-address')
-parser.add_argument('-p', help="The PORT to connect to")
+parser.add_argument('-p', '--port', help="The PORT to connect to")
+parser.add_argument('-k', '--keep-current-playlist', help="Keep current playlist and append if necessary.", action="store_true")
 #parser.add_argument('-p', '--PORT')
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
-
-    if args.A:
-        HOST = args.A
+    if args.address:
+        HOST = args.address
     else:
         HOST = 'localhost'
 
-    if args.p:
-        PORT = args.p
+    if args.port:
+        PORT = args.port
     else:
         PORT = '6600'
 
-    file = args.f
+    file = args.file
     if not os.path.isfile(file):
         print ('{} does not exist.'.format(file))
         sys.exit(1)
 
     mpdjRunner = MPDJRunnerV2(HOST,PORT)
+    
     with open(file, 'r') as loadFile:
         mpdjRunner.mpdj_data = jsonpickle.decode(loadFile.read())
     try:
+        if not args.keep_current_playlist:
+            mpdjRunner.clear_playlist()
         mpdjRunner.run()
     except KeyboardInterrupt:
         print('leaving....')
