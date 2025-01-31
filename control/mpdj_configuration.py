000587ff4fd58fcd4f7e800456b2deaee9563b1f
Björn Gräbe <bjoernuni@gmx.de>
2025-01-31 23:18:18 +0100

    Some more work on the connection configuration.

--- control/mpdj_configuration.py
+++ control/mpdj_configuration.py
@@ -3,6 +3,9 @@ Created on 13.02.2022
 
 @author: Bjoern Graebe
 '''
+
+import copy
+from dns.rdataclass import NONE
 KEY_NAME="Name"
 KEY_HOSTNAME="Hostname"
 KEY_PORT="Port"
@@ -19,10 +22,28 @@ class MPDJConfiguration(object):
 
     def __setstate__(self, p_state):
         self.__dict__.update(p_state)
+        
+    def add_connetion(self, p_name, p_properties):
+        self._connections[p_name]=p_properties
+        
+    def get_connections_copy(self):
+        return copy.deepcopy(self._connections)
+    
+    def get_connection_by_name(self,p_name):
+        if p_name in self._connections:
+            return copy.deepcopy(self._connections[p_name])
+        else:
+            return None
+
+    def remove_connection_by_name(self,p_name):
+        print(self._connections)
+        if p_name in self._connections:
+            self._connections.pop(p_name)
+        print(self._connections) 
 
     def __init__(self):
         '''
         Constructor
         '''
-        self.connections = []
+        self._connections = dict()
         
