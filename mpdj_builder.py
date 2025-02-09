7de72a745e211d872dab7696945b34d5d3501c3d
Björn Gräbe <bjoernuni@gmx.de>
2025-02-09 14:56:32 +0100

    Finished porting to PyQT 6.

--- mpdj_builder.py
+++ mpdj_builder.py
@@ -6,7 +6,7 @@ Created on 12.09.2020
 @author: Bjoern Graebe
 '''
 
-from PyQt5.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication
 from gui.main_window import MainWindowMPDJ
 
 
@@ -15,4 +15,4 @@ if __name__ == '__main__':
     qtApp = QApplication([])
     mw = MainWindowMPDJ()
     mw.show()
-    qtApp.exec_()
+    qtApp.exec()
